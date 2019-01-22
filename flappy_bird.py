b3f609842fb1afd6fa22c5e59403f07f120f69c9
YingLyu <lyuying@bupt.edu.cn>
2019-01-22 22:50:41 +0800

    add Chinese comment

--- flappy_bird.py
+++ flappy_bird.py
@@ -5,36 +5,36 @@ import pygame as pg
 from random import randint
 
 pg.init()
-#设置画板单位
+# 设置画板单位
 side = 32
-#设置画板宽高
+# 设置画板宽高
 w = 17
 h = 15
 disp = pg.display.set_mode((w * side, h * side))
-#设置运行状态
+# 设置运行状态
 game_running = 0
-#设置方向键对应的方向单位坐标
+# 设置方向键对应的方向单位坐标
 dirs = [pg.K_RIGHT, pg.K_DOWN, pg.K_LEFT, pg.K_UP]
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
-#设置初始像素鸟占的坐标
+# 设置初始像素鸟占的坐标
 bird = [(3,4), (2,4), (2,5), (3,5)]
-#柱子的缝隙起始位置
+# 柱子的缝隙起始位置
 pillar_c = []
-#柱子的纵轴坐标二维数组
+# 柱子的纵轴坐标二维数组
 pillar_y = []
-#柱子的横轴坐标一维数组
+# 柱子的横轴坐标一维数组
 pillar_x = []
 
 
-#自由落体时间
+# 自由落体时间
 free_time = 0
-#得分
+# 得分
 point = 0
 
-#生成柱子占据的纵坐标列表
+# 生成柱子占据的纵坐标列表
 def place_pillar(crack_h):
-    #crack_h = randint(0, h - 1 - 5)
+    # crack_h = randint(0, h - 1 - 5)
     pillar = []
 
     for i in range (0, crack_h):
@@ -44,24 +44,24 @@ def place_pillar(crack_h):
     return pillar
 
 
-#柱子缝隙的起始坐标增加新元素
+# 柱子缝隙的起始坐标增加新元素
 pillar_c.append(randint(0, h - 1 - 5))
-#柱子占据的纵坐标列表增加新元素
+# 柱子占据的纵坐标列表增加新元素
 pillar_y.append(place_pillar(pillar_c[-1]))
-#柱子的横坐标增加新元素
+# 柱子的横坐标增加新元素
 pillar_x.append(16)
 
-#检查是否发生碰撞
+# 检查是否发生碰撞
 def check_collisions(): 
-    #对于每个柱子
+    # 对于每个柱子
     for i in range(len(pillar_x)):
-        #判断柱子是否移动到像素小鸟的未知
+        # 判断柱子是否移动到像素小鸟的未知
         if pillar_x[i] == bird[0][0] or pillar_x[i] == bird[1][0]:
-            #判断小鸟最上面的坐标是否在缝隙之上
+            # 判断小鸟最上面的坐标是否在缝隙之上
             if bird[0][1] < pillar_c[i] :  
                 print ("too high")
                 return 3
-            #判断小鸟最下的坐标是否在缝隙之下
+            # 判断小鸟最下的坐标是否在缝隙之下
             if bird[3][1] > pillar_c[i] + 4 :
                 print ("too low")
                 return 3
@@ -71,82 +71,82 @@ clock = pg.time.Clock()
 f = pg.font.SysFont('console', 24, True)
 
 while True:
-    #推进速度
+    # 推进速度
     clock.tick(5)
-    #初始化方向为0
+    # 初始化方向为0
     bird_dirs = 0
-    #获得事件
+    # 获得事件
     for e in pg.event.get():
-        #按下esc停止游戏
+        # 按下esc停止游戏
         if e.type == pg.QUIT:
             pg.quit()
             break
         if e.type == pg.KEYDOWN:
-            #按下按键启动游戏
+            # 按下按键启动游戏
             if game_running == 0:
                 game_runnig = 1
-            #按下空格小鸟方向向上
+            # 按下空格小鸟方向向上
             if e.key == pg.K_SPACE:
                 bird_dirs = 3
             
             for i in range(4):
-                #当按 上或下按键时 更新鸟的方向
+                # 当按 上或下按键时 更新鸟的方向
                 if e.key == dirs[i] :
                     if (i % 2) == 1:
                         bird_dirs = i
 
 
-    #游戏运行状态为2，game over
+    # 游戏运行状态为2，game over
     if game_running == 2:
-        #disp.fill(pg.Color("black"))
+        # disp.fill(pg.Color("black"))
         disp.blit(f.render("Game over!", False, pg.Color("red")), (3 * side, 3 * side))
         disp.blit(f.render(str(point), False, pg.Color("green")), (13 * side, 3 * side))
         pg.display.update()
         game_running = 3
-    #游戏运行状态为2，终止循环
+    # 游戏运行状态为2，终止循环
     if game_running == 3:
         continue  
-    #小鸟方向不为0时，进行自由落体
+    # 小鸟方向不为0时，进行自由落体
     if bird_dirs != 0:
-        #小鸟方向3为向上
+        # 小鸟方向3为向上
         if bird_dirs == 3:
-            #将自由落体时间重置为0
+            # 将自由落体时间重置为0
             free_time = 0
-        #根据选择的向上或向下移动小鸟
+        # 根据选择的向上或向下移动小鸟
         for j in range(4):
             bird.append((bird[j][0] + dx[bird_dirs], bird[j][1] + dy[bird_dirs]))
-        #print (bird)
+        # print (bird)
         for j in range(4):
             bird.pop(0)  
-        #print (bird)
+        # print (bird)
 
-    #自由落体时间增加
+    # 自由落体时间增加
     free_time = free_time + 1
     
-    #根据自由落体公式得到第 free_time 个单位时间内移动的距离，移动小鸟
+    # 根据自由落体公式得到第 free_time 个单位时间内移动的距离，移动小鸟
     for j in range(4):
         bird.append((bird[j][0] , bird[j][1] + free_time * 0.25 - 0.125))
     for j in range(4):
         bird.pop(0)  
 
-	#对于每一个柱子
+	# 对于每一个柱子
     for i in range(len(pillar_x)):
-        #改变柱子的横坐标，向左移动柱子
+        # 改变柱子的横坐标，向左移动柱子
         if pillar_x[i] > 0:
             pillar_x[i] = pillar_x[i] - 1
             
         else:
-            #当柱子的移除画面外时，删除柱子
+            # 当柱子的移除画面外时，删除柱子
             pillar_c.pop(0)
             pillar_x.pop(0)
             pillar_y.pop(0)
-            #创建新的柱子
+            # 创建新的柱子
             pillar_c.append(randint(0, h - 1 - 5))
             pillar_y.append(place_pillar(pillar_c[-1]))
             pillar_x.append(16)
-            #增加分数
+            # 增加分数
             point = point + 1
-    #背景填充
+    # 背景填充
     disp.fill(pg.Color("black")) 
 
     # 画像素鸟
@@ -162,7 +162,7 @@ while True:
     if coll >= 2:
         game_running = 2
     else:
-        #展示分数
+        # 展示分数
         disp.blit(f.render(str(point), False, pg.Color("green")), (13 * side, 3 * side))
-        #刷新画板
+        # 刷新画板
         pg.display.update()
--- tetris.py
+++ tetris.py
@@ -1,10 +1,5 @@
 #俄罗斯方块
 
-# Tetromino (a Tetris clone)
-# By Al Sweigart al@inventwithpython.com
-# http://inventwithpython.com/pygame
-# Released under a "Simplified BSD" license
-
 import random, time, pygame
 
 
@@ -41,7 +36,7 @@ TEXTCOLOR = WHITE
 TEXTSHADOWCOLOR = GRAY
 COLORS      = (     BLUE,      GREEN,      RED,      YELLOW)
 LIGHTCOLORS = (LIGHTBLUE, LIGHTGREEN, LIGHTRED, LIGHTYELLOW)
-assert len(COLORS) == len(LIGHTCOLORS) # each color must have light color
+assert len(COLORS) == len(LIGHTCOLORS) # 每种颜色有对应的亮色
 
 TEMPLATEWIDTH = 5
 TEMPLATEHEIGHT = 5
@@ -198,25 +193,25 @@ def runGame():
     fallingPiece = getNewPiece()    
     nextPiece = getNewPiece()
 
-    while True: # game loop
+    while True: # 游戏循环
         if fallingPiece == None:
-            # No falling piece in play, so start a new piece at the top
+            # 如果没有下落的形状，就在最上面增加之前显示的“下一个形状”，同时获得新的“下一个形状”
             fallingPiece = nextPiece
             nextPiece = getNewPiece()
-            lastFallTime = time.time() # reset lastFallTime
+            lastFallTime = time.time() # 重置 下降开始时间
 
             if not isValidPosition(board, fallingPiece):
-                return # can't fit a new piece on the board, so game over
+                return # 如果下降的形状在非法的位置，游戏结束
         
         checkForQuit()
         
-        for event in pygame.event.get(): # event handling loop
+        for event in pygame.event.get(): # 处理事件循环
             if event.type == pygame.KEYUP:
                 if (event.key == pygame.K_p):
-                    # Pausing the game
+                    # 暂停游戏
                     DISPLAYSURF.fill(BGCOLOR)
                     pygame.mixer.music.stop()
-                    showTextScreen('Paused') # pause until a key press
+                    showTextScreen('Paused') # 按下按键后继续
                     pygame.mixer.music.play(-1, 0.0)
                     lastFallTime = time.time()
                     lastMoveDownTime = time.time()
@@ -229,8 +224,8 @@ def runGame():
                     movingDown = False
 
             elif event.type == pygame.KEYDOWN:
-                # moving the piece sideways
-                if event.key == pygame.K_ESCAPE: #ying
+                # 左右移动形状
+                if event.key == pygame.K_ESCAPE: # ying
                     terminate()
                 elif (event.key == pygame.K_LEFT or event.key == pygame.K_a) and isValidPosition(board, fallingPiece, adjX=-1):
                     fallingPiece['x'] -= 1
@@ -244,7 +239,7 @@ def runGame():
                     movingLeft = False
                     lastMoveSidewaysTime = time.time()
 
-                # rotating the piece (if there is room to rotate)
+                # （如果空间允许）旋转形状
                 elif (event.key == pygame.K_UP or event.key == pygame.K_w):
                     fallingPiece['rotation'] = (fallingPiece['rotation'] + 1) % len(PIECES[fallingPiece['shape']])
                     if not isValidPosition(board, fallingPiece):
@@ -254,7 +249,7 @@ def runGame():
                     if not isValidPosition(board, fallingPiece):
                         fallingPiece['rotation'] = (fallingPiece['rotation'] + 1) % len(PIECES[fallingPiece['shape']])
 
-                # making the piece fall faster with the down key
+                # 按向下键，下落速度加快
                 elif (event.key == pygame.K_DOWN or event.key == pygame.K_s):
                     movingDown = True
                     if isValidPosition(board, fallingPiece, adjY=1):
@@ -271,7 +266,7 @@ def runGame():
                             break
                     fallingPiece['y'] += i - 1
         
-        # handle moving the piece because of user input
+        # 处理用户输入的移动命令
         if (movingLeft or movingRight) and time.time() - lastMoveSidewaysTime > MOVESIDEWAYSFREQ:
             if movingLeft and isValidPosition(board, fallingPiece, adjX=-1):
                 fallingPiece['x'] -= 1
@@ -283,21 +278,21 @@ def runGame():
             fallingPiece['y'] += 1
             lastMoveDownTime = time.time()
         
-        # let the piece fall if it is time to fall
+        # 根据速度设置，如果到了下降的时间，形状下降
         if time.time() - lastFallTime > fallFreq:
-            # see if the piece has landed
+            # 判断形状是否着陆
             if not isValidPosition(board, fallingPiece, adjY=1):
-                # falling piece has landed, set it on the board
+                # 如果形状着陆，将其固定在板子上
                 addToBoard(board, fallingPiece)
                 score += removeCompleteLines(board)
                 level, fallFreq = calculateLevelAndFallFreq(score)
                 fallingPiece = None
             else:
-                # piece did not land, just move the piece down
+                # 如果形状没有着陆，正常下降
                 fallingPiece['y'] += 1
                 lastFallTime = time.time()
         
-        # drawing everything on the screen
+        # 将所有元素绘制在屏幕上
         DISPLAYSURF.fill(BGCOLOR)
         drawBoard(board)
         drawStatus(score, level)
@@ -308,20 +303,20 @@ def runGame():
         pygame.display.update()
         FPSCLOCK.tick(FPS)
         
-#创建文本对象
+# 创建文本对象
 def makeTextObjs(text, font, color):
     surf = font.render(text, True, color)
     return surf, surf.get_rect()
 
-#结束处理
+# 结束处理
 def terminate():
     pygame.quit()
 
 
 
 def checkForKeyPress():
-    # Go through event queue looking for a KEYUP event.
-    # Grab KEYDOWN events to remove them from the event queue.
+    # 浏览事件队列以查找 按键松开 事件。
+    # 抓取 按键按下 事件以将其从事件队列中删除。
     checkForQuit()
 '''
     for event in pygame.event.get([pygame.KEYDOWN, pygame.KEYUP]):
@@ -332,20 +327,19 @@ def checkForKeyPress():
 '''
 
 def showTextScreen(text):
-    # This function displays large text in the
-    # center of the screen until a key is pressed.
+    # 此函数在屏幕中心显示大文本，直到按下一个键。
     
-    # Draw the text drop shadow
+    # 绘制文本投影
     titleSurf, titleRect = makeTextObjs(text, BIGFONT, TEXTSHADOWCOLOR)
     titleRect.center = (int(WINDOWWIDTH / 2), int(WINDOWHEIGHT / 2))
     DISPLAYSURF.blit(titleSurf, titleRect.center)
 
-    # Draw the text
+    # 绘制文字
     titleSurf, titleRect = makeTextObjs(text, BIGFONT, TEXTCOLOR)
     titleRect.center = (int(WINDOWWIDTH / 2) - 3, int(WINDOWHEIGHT / 2) - 3)
     DISPLAYSURF.blit(titleSurf, titleRect.center)
 
-    # Draw the additional "Press a key to play." text.
+    # 画出额外的“按一个键进行播放”。
     pressKeySurf, pressKeyRect = makeTextObjs('Press a key to play.', BASICFONT, TEXTCOLOR)
     pressKeyRect.center = (int(WINDOWWIDTH / 2), int(WINDOWHEIGHT / 2) + 100)
     DISPLAYSURF.blit(pressKeySurf, pressKeyRect.center)
@@ -356,12 +350,12 @@ def showTextScreen(text):
         FPSCLOCK.tick()
     '''
     
-#检查是否为结束状态
+# 检查是否为结束状态
 def checkForQuit():
     
-    for event in pygame.event.get(pygame.QUIT): # get all the QUIT events
+    for event in pygame.event.get(pygame.QUIT): #获取所有QUIT事件
         
-        terminate() # terminate if any QUIT events are present
+        terminate() # 如果存在任何QUIT事件，则终止
         
     
     '''
@@ -369,29 +363,29 @@ def checkForQuit():
         if event.key == pygame.K_ESCAPE:
             terminate() # terminate if the KEYUP event was for the Esc key
         
-        #pygame.event.post(event) # put the other KEYUP event objects back
+        # pygame.event.post(event) # put the other KEYUP event objects back
         
     '''
-#计算等级以及对应的下降速度
+# 计算等级以及对应的下降速度
 def calculateLevelAndFallFreq(score):
-    # Based on the score, return the level the player is on and
-    # how many seconds pass until a falling piece falls one space.
+    # 根据分数，返回玩家所在的等级
+    # 以及经过多少秒钟，直到掉落的一块空间落下。
     level = int(score / 10) + 1
     fallFreq = 0.27 - (level * 0.02)
     return level, fallFreq
 
-#生成新的形状
+# 生成新的形状
 def getNewPiece():
-    # return a random new piece in a random rotation and color
+    # 以随机旋转和颜色返回一个随机的新片段
     shape = random.choice(list(PIECES.keys()))
     newPiece = {'shape': shape,
                 'rotation': random.randint(0, len(PIECES[shape]) - 1),
                 'x': int(BOARDWIDTH / 2) - int(TEMPLATEWIDTH / 2),
-                'y': -2, # start it above the board (i.e. less than 0)
+                'y': -2, #在板子上方开始（即小于0）
                 'color': random.randint(0, len(COLORS)-1)}
     return newPiece
 
-#将形状加入游戏区域
+# 将形状加入游戏区域
 def addToBoard(board, piece):
     # fill in the board based on piece's location, shape, and rotation
     for x in range(TEMPLATEWIDTH):
@@ -399,21 +393,21 @@ def addToBoard(board, piece):
             if PIECES[piece['shape']][piece['rotation']][y][x] != BLANK:
                 board[x + piece['x']][y + piece['y']] = piece['color']
 
-#创建空的游戏区域 数据结构
+# 创建空的游戏区域 数据结构
 def getBlankBoard():
-    # create and return a new blank board data structure
+    # 根据形状的位置，形状和旋转填充板
     board = []
     for i in range(BOARDWIDTH):
         board.append([BLANK] * BOARDHEIGHT)
     return board
 
-#判断形状在游戏区域内
+# 判断形状在游戏区域内
 def isOnBoard(x, y):
     return x >= 0 and x < BOARDWIDTH and y < BOARDHEIGHT
 
-#判断形状的位置是否合法
+# 判断形状的位置是否合法
 def isValidPosition(board, piece, adjX=0, adjY=0):
-    # Return True if the piece is within the board and not colliding
+    # 如果棋子在棋盘内并且没有碰撞，则返回True
     for x in range(TEMPLATEWIDTH):
         for y in range(TEMPLATEHEIGHT):
             isAboveBoard = y + piece['y'] + adjY < 0
@@ -424,48 +418,48 @@ def isValidPosition(board, piece, adjX=0, adjY=0):
             if board[x + piece['x'] + adjX][y + piece['y'] + adjY] != BLANK:
                 return False
     return True
-#判断一行填满
+# 判断一行填满
 def isCompleteLine(board, y):
-    # Return True if the line filled with boxes with no gaps.
+    # R如果行填充没有间隙的行，则返回True。
     for x in range(BOARDWIDTH):
         if board[x][y] == BLANK:
             return False
     return True
 
-#消除填满的行
+# 消除填满的行
 def removeCompleteLines(board):
-    # Remove any completed lines on the board, move everything above them down, and return the number of complete lines.
+    # 移除板上已完成的所有行，将其上方的所有行向下移动，并返回完整线条的数量。
     numLinesRemoved = 0
-    y = BOARDHEIGHT - 1 # start y at the bottom of the board
+    y = BOARDHEIGHT - 1 # y轴从板底部开始
     while y >= 0:
         if isCompleteLine(board, y):
-            # Remove the line and pull boxes down by one line.
+            # 删除完成的行并将框向下拉一行。
             for pullDownY in range(y, 0, -1):
                 for x in range(BOARDWIDTH):
                     board[x][pullDownY] = board[x][pullDownY-1]
-            # Set very top line to blank.
+            # 将最上面的行设置为空白。
             for x in range(BOARDWIDTH):
                 board[x][0] = BLANK
             numLinesRemoved += 1
-            # Note on the next iteration of the loop, y is the same.
-            # This is so that if the line that was pulled down is also
-            # complete, it will be removed.
+            # 注意循环的下一次迭代，y是相同的。
+            # 这样如果被拉下的行也是如此
+            # 完成后，它将被删除。
         else:
-            y -= 1 # move on to check next row up
+            y -= 1 # 继续检查下一行
     return numLinesRemoved
 
-#像素转换
+# 像素转换
 def convertToPixelCoords(boxx, boxy):
-    # Convert the given xy coordinates of the board to xy
-    # coordinates of the location on the screen.
+    # 将给定的板子xy坐标转换为真正的xy
+    # 屏幕上位置的坐标。
     return (XMARGIN + (boxx * BOXSIZE)), (TOPMARGIN + (boxy * BOXSIZE))
 
-#画组成形状的格子
+# 画组成形状的格子
 def drawBox(boxx, boxy, color, pixelx=None, pixely=None):
-    # draw a single box (each tetromino piece has four boxes)
-    # at xy coordinates on the board. Or, if pixelx & pixely
-    # are specified, draw to the pixel coordinates stored in
-    # pixelx & pixely (this is used for the "Next" piece).
+    # 画一个盒子（每个tetromino片有四个盒子）
+    # 在板上的xy坐标处。或者，如果pixelx＆pixely
+    # 指定, 绘制到存储的像素坐标
+    # pixelx＆pixely（这用于“下一个形状”）。
     if color == BLANK:
         return
     if pixelx == None and pixely == None:
@@ -473,55 +467,55 @@ def drawBox(boxx, boxy, color, pixelx=None, pixely=None):
     pygame.draw.rect(DISPLAYSURF, COLORS[color], (pixelx + 1, pixely + 1, BOXSIZE - 1, BOXSIZE - 1))
     pygame.draw.rect(DISPLAYSURF, LIGHTCOLORS[color], (pixelx + 1, pixely + 1, BOXSIZE - 4, BOXSIZE - 4))
 
-#画出游戏区域
+# 画出游戏区域
 def drawBoard(board):
-    # draw the border around the board
+    # 画出板子边框
     pygame.draw.rect(DISPLAYSURF, BORDERCOLOR, (XMARGIN - 3, TOPMARGIN - 7, (BOARDWIDTH * BOXSIZE) + 8, (BOARDHEIGHT * BOXSIZE) + 8), 5)
 
-    # fill the background of the board
+    # 填写板子的背景
     pygame.draw.rect(DISPLAYSURF, BGCOLOR, (XMARGIN, TOPMARGIN, BOXSIZE * BOARDWIDTH, BOXSIZE * BOARDHEIGHT))
-    # draw the individual boxes on the board
+    # 画出板子上的各个盒子
     for x in range(BOARDWIDTH):
         for y in range(BOARDHEIGHT):
             drawBox(x, y, board[x][y])
 
-#显示得分等级信息
+# 显示得分等级信息
 def drawStatus(score, level):
-    # draw the score text
+    # 绘制得分文本
     
     scoreSurf = BASICFONT.render('Score: %s' % score, True, TEXTCOLOR)
     scoreRect = scoreSurf.get_rect()
     scoreRect.topleft = (WINDOWWIDTH - 150, 20)
     DISPLAYSURF.blit(scoreSurf, scoreRect.topleft)
 
-    # draw the level text
+    # 绘制关卡文字
     levelSurf = BASICFONT.render('Level: %s' % level, True, TEXTCOLOR)
     levelRect = levelSurf.get_rect()
     levelRect.topleft = (WINDOWWIDTH - 150, 50)
     DISPLAYSURF.blit(levelSurf, levelRect.topleft)
     
-#画出形状
+# 画出形状
 def drawPiece(piece, pixelx=None, pixely=None):
     shapeToDraw = PIECES[piece['shape']][piece['rotation']]
     if pixelx == None and pixely == None:
-        # if pixelx & pixely hasn't been specified, use the location stored in the piece data structure
+        # 如果未指定pixelx和pixely，请使用存储在片段数据结构中的位置
         pixelx, pixely = convertToPixelCoords(piece['x'], piece['y'])
 
-    # draw each of the boxes that make up the piece
+    # 绘制构成该块的每个盒子
     for x in range(TEMPLATEWIDTH):
         for y in range(TEMPLATEHEIGHT):
             if shapeToDraw[y][x] != BLANK:
                 drawBox(None, None, piece['color'], pixelx + (x * BOXSIZE), pixely + (y * BOXSIZE))
 
-#画出下一个形状的提醒
+# 画出下一个形状的提醒
 def drawNextPiece(piece):
-    # draw the "next" text
+    # 绘制“next”文本
     nextSurf = BASICFONT.render('Next:', True, TEXTCOLOR)
     nextRect = nextSurf.get_rect()
     nextRect.topleft = (WINDOWWIDTH - 120, 80)
     DISPLAYSURF.blit(nextSurf, nextRect.topleft)
     
-    # draw the "next" piece
+    # 画出“下一个”形状
     drawPiece(piece, pixelx=WINDOWWIDTH-120, pixely=100)
     
 
